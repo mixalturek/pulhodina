13c2656f9718878b8e65ba94f46dea3032ae26d1
Michal Turek <mixalturek@users.sf.net>
2014-12-06 19:26:14 +0100

    Parsing input data file to internal representation.

--- src/pulhodina.py
+++ src/pulhodina.py
@@ -37,6 +37,7 @@ import re
 VERSION = '0.1.0-SNAPSHOT'
 INPUT_FILE_ENCODING='utf-16-le'
 OUTPUT_FILE_ENCODING='utf-8'
+RECORDS_DELIMITER="\t"
 
 
 ###############################################################################
@@ -89,10 +90,120 @@ def parse_arguments(argv):
 def debug_show_arguments(argv, arguments):
     """Show parsed command line arguments if verbose is enabled."""
     if arguments.verbose:
-        print("Raw arguments:    {0}".format(argv))
-        print("Verbose:          {0}".format(arguments.verbose))
-        print("Input directory:  {0}".format(arguments.input_dir))
-        print("Output directory: {0}".format(arguments.output_dir))
+        print("Raw arguments:   ", argv)
+        print("Verbose:         ", arguments.verbose)
+        print("Input directory: ", arguments.input_dir)
+        print("Output directory:", arguments.output_dir)
+
+
+###############################################################################
+####
+
+class Record(object):
+    """Data of a record."""
+
+    def __init__(self, line):
+        """Constructor."""
+        parts = [part.strip() for part in line.split(RECORDS_DELIMITER)]
+
+        try:
+            self.sorg         = parts[0]
+            self.cred_acct    = parts[1]
+            self.name1        = parts[2]
+            self.po_number    = parts[3]
+            self.credit_value = parts[4]
+            self.open_del     = parts[5]
+            self.receivables  = parts[6]
+            self.special_liab = parts[7]
+            self.cred_limit   = parts[8]
+            self.use          = parts[9]
+            self.next_date    = parts[10]
+        except IndexError as e:
+            print("ERROR: Too less parts for record, skipping:", parts, file=sys.stderr)
+
+    def __str__(self):
+        """Format object to a string."""
+        attrs = vars(self)
+        return ', '.join("'{0}'".format(v[1]) for v in attrs.items())
+
+
+###############################################################################
+####
+
+class Section(object):
+    """Data stored in a section."""
+
+    def __init__(self):
+        """Constructor."""
+        self.records = []
+
+    def __str__(self):
+        """Format object to a string."""
+        return "\n".join([str(r) for r in self.records])
+
+    def add_record(self, record):
+        """Add a new record."""
+        self.records.append(record)
+
+    def is_empty(self):
+        """Is the section empty?"""
+        return len(self.records) == 0
+
+
+###############################################################################
+####
+
+class DataFile(object):
+    """Data stored in a file."""
+
+    def __init__(self):
+        """Constructor."""
+        self.sections = []
+
+    def __str__(self):
+        """Format object to a string."""
+        return "\n\n".join([str(s) for s in self.sections])
+
+    def add_section(self, section):
+        """Add a new section."""
+        if section.is_empty():
+            return
+
+        self.sections.append(section)
+
+
+###############################################################################
+####
+
+class Parser(object):
+    """Parse input data to the internal representation."""
+
+    def __init__(self):
+        """Constructor."""
+        pass
+
+    def parse(self, lines):
+        data_file = DataFile()
+        current_section = Section()
+
+        for line in lines[2:]:
+            line = line.strip()
+
+            if len(line) == 0:
+                continue
+
+            if line.startswith('*'):
+                data_file.add_section(current_section)
+                current_section = Section()
+                continue
+
+            record = Record(line)
+            current_section.add_record(record)
+
+        # Add unfinished one if any
+        data_file.add_section(current_section)
+
+        return data_file
 
 
 ###############################################################################
@@ -105,9 +216,9 @@ class VerySpecialHtmlFormatter(object):
         """Constructor."""
         pass
 
-    def format(self, lines):
-        """Apply tranformation and format lines to HTML table."""
-        return lines
+    def format(self, data_file):
+        """Apply tranformation and format data to HTML table."""
+        return str(data_file)
 
 
 ###############################################################################
@@ -126,13 +237,14 @@ def format_one_file(input_path, output_path):
     with open(input_path, mode='r', encoding=INPUT_FILE_ENCODING) as fr:
         input_lines = fr.readlines()
 
+    parser = Parser()
+    data_file = parser.parse(input_lines)
+
     formatter = VerySpecialHtmlFormatter()
-    output_lines = formatter.format(input_lines)
+    output = formatter.format(data_file)
 
     with open(output_path, mode='w', encoding=OUTPUT_FILE_ENCODING) as fw:
-        for line in output_lines:
-            fw.write(line)
-            fw.write("\n")
+        fw.write(output)
 
 
 ###############################################################################
@@ -146,7 +258,7 @@ def format_multiple_files(args, file_names):
         output_path = re.sub(r'[^.]+$', 'html', output_path)
 
         if args.verbose:
-            print("{0} -> {1}".format(input_path, output_path))
+            print(input_path, "->", output_path)
 
         format_one_file(input_path, output_path)
 
